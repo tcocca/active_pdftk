3c338a98338dc05ef4b3ecff889845ca7875011a
Mike Auclair <mauclair@nicusa.com>
2011-10-19 15:05:47 -0400

    caches pdftk location at class level

--- lib/active_pdftk/call.rb
+++ lib/active_pdftk/call.rb
@@ -298,10 +298,16 @@ module ActivePdftk
     #
     # @note Should work on all unix systems (Linux, Mac Os X)
     #
-    def locate_pdftk
+    
+    def self.locate_pdftk
+      return @pdftk_located if @pdftk_located
       auto_path = %x{locate pdftk | grep "/bin/pdftk"}.strip.split("\n").first
       #TODO find a valid Win32 procedure (not in my top priorities)
-      (auto_path.nil? || auto_path.empty?) ? nil : auto_path
+      @pdftk_located = (auto_path.nil? || auto_path.empty?) ? nil : auto_path
+    end
+    
+    def locate_pdftk
+      self.class.locate_pdftk
     end
 
     private
