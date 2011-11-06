b042e3a081cdec16f11041dbb1108d225eceb5b0
Tom Cocca <tom.cocca@gmail.com>
2011-11-06 08:52:14 -0500

    embed a pdftk that will potentially be used for testing on travis-ci, maybe?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,13 @@ require File.expand_path('../../lib/active_pdftk', __FILE__)
 require 'rspec'
 
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
+
+=begin
+RSpec.configure do |config|
+  config.before(:each) do
+    if ENV['embeded_pdftk'] == "true"
+      ActivePdftk::Call.any_instance.stub(:locate_pdftk).and_return(File.dirname(__FILE__) + '/support/pdftk')
+    end
+  end
+end
+=end
