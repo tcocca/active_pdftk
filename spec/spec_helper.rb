8d924960a88097f07514a66fa9386a1165a60707
Tom Cocca <tom.cocca@gmail.com>
2011-11-06 18:07:52 -0500

    spec-ed cat and shuffle commands

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,12 +4,10 @@ require 'rspec'
 
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
 
-=begin
 RSpec.configure do |config|
   config.before(:each) do
     if ENV['embeded_pdftk'] == "true"
-      ActivePdftk::Call.any_instance.stub(:locate_pdftk).and_return(File.dirname(__FILE__) + '/support/pdftk')
+      ActivePdftk::Call.stub(:locate_pdftk).and_return(File.dirname(__FILE__) + '/support/pdftk')
     end
   end
 end
-=end
