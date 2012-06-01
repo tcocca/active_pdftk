edcee85b55814d9850e8e5e069d658ac42ea215f
Tom Cocca <tom.cocca@gmail.com>
2012-06-01 00:52:01 -0400

    removed the embeded spec binary because travis-ci will let us install packages through apt now so we should never need to try and use this binary in the specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,12 +5,6 @@ require 'rspec'
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
 
 RSpec.configure do |config|
-  config.before(:each) do
-    if ENV['embeded_pdftk'] == "true"
-      ActivePdftk::Call.stub(:locate_pdftk).and_return(File.dirname(__FILE__) + '/support/pdftk')
-    end
-  end
-
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
 end
